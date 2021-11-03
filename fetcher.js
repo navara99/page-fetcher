2aebde050fe45dc10e5f5454318f0a538494e68b
Tharsikan <thar.navaratnam@gmail.com>
2021-11-03 20:23:28 +0000

    eslint fix

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,10 @@
-const fs = require('fs')
+const fs = require('fs');
 const request = require('request');
 const args = process.argv.splice(2);
 
 const showError = (err) => {
   console.log(`Error: ${err}`);
-}
+};
 
 const downloadPage = (input) => {
   const url = input[0];
@@ -15,28 +15,28 @@ const downloadPage = (input) => {
     if (err) {
       console.log(`Error: ${err} \n Status code: ${res}`);
       return;
-    };
+    }
 
     fs.writeFile(path, body, (err) => {
       if (err) {
         showError(err);
-        return
-      };
+        return;
+      }
 
       fs.stat(path, (err, stats) => {
         if (err) {
           showError(err);
-          return
+          return;
         }
 
-        console.log(`Downloaded and saved ${stats.size} bytes to ${path}.`)
-      })
+        console.log(`Downloaded and saved ${stats.size} bytes to ${path}.`);
+      });
 
-    })
+    });
 
   });
 
 };
 
-downloadPage(args)
+downloadPage(args);
 
