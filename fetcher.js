eddcc6ef37fd5541220768b5c9249f3bda8a8397
Tharsikan <thar.navaratnam@gmail.com>
2021-11-03 19:59:29 +0000

    working base case solution

---
+++ fetcher.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const request = require('request');
+const args = process.argv.splice(2);
+
+const showError = (err) => {
+  console.log(`Error: ${err}`);
+}
+
+const downloadPage = (input) => {
+  const url = input[0];
+  const path = input[1];
+
+  request(url, (err, res, body) => {
+
+    if (err) {
+      console.log(`Error: ${err} \n Status code: ${res}`);
+      return;
+    };
+
+    fs.writeFile(path, body, (err) => {
+      if (err) {
+        showError(err);
+        return
+      };
+
+      fs.stat(path, (err, stats) => {
+        if (err) {
+          showError(err);
+          return
+        }
+
+        console.log(`Downloaded and saved ${stats.size} bytes to ${path}.`)
+      })
+
+    })
+
+  });
+
+};
+
+downloadPage(args)
+
